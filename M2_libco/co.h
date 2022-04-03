23f539b05f0eb8fe687388ab6c68f598182200e1
Jacen-cpu <3163083993@qq.com>
2022-04-03 21:52:40 +0800

    new

--- M2_libco/co.h
+++ M2_libco/co.h
@@ -2,7 +2,22 @@ struct co* co_start(const char* name, void (*func)(void *), void *arg);
 void co_yield();
 void co_wait(struct co *co);
 
-struct co {
-	
+enum co_status {
+	CO_NEW = 1,
+	CO_RUNNING,
+	CO_WAITING,
+	CO_DEAD,
 };
+
+struct co {
+	char* name;
+	void (*func) (void *);
+	void *arg;
+
+	enum co_status status;
+	struct co *waiter;
+	jmp_buf context;
+	uint8_t stack[STACK_SIZE];
+};
+
 
